02ab1f9ecfb87e440813f271e3ce8b7fb183133a
Iain Nash <iain@codejoust.com>
2015-09-26 07:51:59 -0400

    Initial Commit

---
+++ MedCheck/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MedCheck
+//
+//  Created by Iain Nash on 9/26/15.
+//  Copyright © 2015 Iain Nash. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
