7ae341751b4247b8e530a313569aa237e69411f9
Iain Nash <iain@codejoust.com>
2015-09-26 12:54:56 -0400

    Adding provider data

--- MedCheck/FirstViewController.swift
+++ MedCheck/FirstViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import Charts
+import Foundation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, ChartViewDelegate {
 
+  @IBOutlet var chartView: LineChartView!
+  
   override func viewDidLoad() {
+    chartView.descriptionText = "data from heart monitor sensor"
+    chartView.noDataText = "no data yet"
+    chartView.pinchZoomEnabled = false
+    chartView.dragEnabled = true
+    chartView.drawGridBackgroundEnabled = false
+    
+    chartView.legend.enabled = false
+    chartView.leftAxis.enabled = false
+    chartView.rightAxis.enabled = false
+    chartView.xAxis.enabled = false
+    
+    
+    chartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+    setData()
+    
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
 
+  func setData() {
+    let yvals: NSMutableArray = NSMutableArray()
+    for index in 1...100 {
+      yvals.addObject(BarChartDataEntry(value: 2 * sin(Double(index)), xIndex: index))
+    }
+    let xvals: NSMutableArray = NSMutableArray()
+    for index in 1...100 {
+      xvals.addObject(String(index))
+    }
+    
+    let set1 : LineChartDataSet = LineChartDataSet(yVals: (yvals as NSArray as! [ChartDataEntry]), label: "Dataset 1")
+    set1.drawCubicEnabled = true
+    set1.cubicIntensity = 0.2
+    set1.drawCirclesEnabled = false
+    set1.lineWidth = 2.0
+    set1.highlightColor = UIColor(red: 0.2, green: 0.5, blue: 0.9, alpha: 1.0)
+    let data : LineChartData = LineChartData(xVals: xvals as NSArray as! [String], dataSet: set1)
+    chartView.data = data;
+    chartView.setNeedsDisplay()
+    chartView.notifyDataSetChanged()
+    chartView.delegate = self
+    
+  }
+  
+  
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'MedCheck' do
+  use_frameworks!
+  pod 'AFNetworking', '~> 2.5'
+  pod 'SwiftyJSON'
+  pod 'Charts'
+end
+
