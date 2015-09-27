37306a6c5aaf8c2d1836c41c52ef5f0aa45bdd2d
khirani6 <khirani6@gatech.edu>
2015-09-26 21:47:49 -0400

    Commit

--- MedCheck/SecondViewController.swift
+++ MedCheck/SecondViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    //MARK: Properties
+    @IBOutlet weak var codeTextField: UIView!
+    @IBOutlet weak var inviteCodeLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //MARK: Actions
+    
 
 }
 
